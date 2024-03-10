9c8438bd08167331e0ab8d5c8d0b726254c061d4
thdud <thgml1444@naver.com>
2024-03-10 13:59:56 +0900

    Update : 버전 및 의존성 문제로 새로 프로젝트 시작 - next.js v13 antd v4

---
+++ components/AppLayout.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import { Menu, Input, Button } from "antd";
+
+const AppLayout = ({ children }) => {
+  return (
+    <div>
+      <Menu mode="horizontal">
+        <Menu.Item key="home">
+          <Link href="/">노드버드</Link>
+        </Menu.Item>
+        <Menu.Item key="profile">
+          <Link href="/profile">프로필</Link>
+        </Menu.Item>
+        <Menu.Item key="mail">
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+      </Menu>
+      <Link href="/signup">
+        <Button>회원가입</Button>
+      </Link>
+      {children}
+    </div>
+  );
+};
+
+AppLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+export default AppLayout;
---
+++ pages/signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+import AppLayout from "../components/AppLayout";
+
+const Signup = () => (
+  <AppLayout>
+    <Head>
+      <title>회원가입</title>
+    </Head>
+    <div>회원가입페이지 </div>
+  </AppLayout>
+);
+
+export default Signup;
