908f0600d28a1776be45af988f081d6696dc7912
thdud <thgml1444@naver.com>
2024-03-25 11:14:18 +0900

    Add : 로그인 하지 않았을 경우, 프로필 들어가지 못하게 router설정

--- pages/profile.js
+++ pages/profile.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import Router from "next/router";
 
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
@@ -8,6 +9,14 @@ import NicknameEditForm from "../components/NicknameEditForm";
 
 const Profile = () => {
   const { me } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (!(me && me.id)) {
+      Router.push("/");
+    }
+  }, [me && me.id]);
+
+  if (!me) return null;
 
   return (
     <AppLayout>
