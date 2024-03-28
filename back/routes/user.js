7e3a6ec5d9eb3f8a2ff390017e473f29fc5022ea
thdud <thgml1444@naver.com>
2024-03-28 14:45:31 +0900

    Update : backend의 user라우터 기능 추가 - 로그인, 로그아웃, 회원가입업데이트 - express의 모듈을 확장해 passport 미들웨어를 추가하여 로그인 기능 추가. passport의 결과값으로 프론트에 응답값을 전달한다. - passport의 login을 한번 더 시도하여 로그인한 사용자 데이터를 저장해둔다.

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const passport = require("passport");
 const { User } = require("../models");
 const router = express.Router();
 
+// POST /user/login
+router.post("/login", (req, res, next) => {
+
+  // 미들웨어 확장해서 passport사용하기.
+  passport.authenticate("local", (serverError, user, clientError) => {
+    if (serverError) {
+      console.error(serverError);
+      return next(serverError);
+    }
+    if (clientError) {
+      return res.status(401).send(clientError.reason);
+    }
+
+    // passport에서 로그인을 한번 더
+    return req.login(user, async (loginErr) => {
+      if (loginErr) {
+        console.error(loginErr);
+        return next(loginErr);
+      }
+
+      return res.status(200).json(user);
+    });
+  })(req, res, next);
+});
+
+// POST /user/
 router.post("/", async (req, res, next) => {
-  // POST /user/
-  console.log("signup서버요청중");
   try {
     const exUser = await User.findOne({
       where: {
@@ -21,12 +46,21 @@ router.post("/", async (req, res, next) => {
       password: hashedPassword,
       nickname: req.body.nickname,
     });
+
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.status(200).send("signup OK");
+
   } catch (error) {
     console.error(error);
     next(error); //status(500)
   }
+});
+
+// POST /user/logout
+router.post("/logout", (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.send("logout OK");
 });
 
 module.exports = router;
