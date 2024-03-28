492c4b267dfaee9d68fc8f0630ae3eaa18c529de
thdud <thgml1444@naver.com>
2024-03-28 14:46:51 +0900

    Add : backend server의 설정파일인 app에 미들웨어 require 설정해둠 - 세션, 쿠키, passport, dotenv.

--- back/app.js
+++ back/app.js
@@ -1,17 +1,26 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const dotenv = require("dotenv");
 
+const db = require("./models");
+const passportCofig = require("./passport");
 const postRouter = require("./routes/post");
 const userRouter = require("./routes/user");
-const db = require("./models");
 
+dotenv.config();
+const app = express();
 db.sequelize
   .sync()
   .then(() => {
     console.log("db연결성공");
   })
   .catch(console.error);
+
+// login 설정
+passportCofig();
 
 // use 안에 들어가는 것들 => 미들웨어
 app.use(
@@ -20,27 +29,21 @@ app.use(
     credentials: false,
   })
 );
-// express 서버실행. app의 제일 처음으로 작성해줘야 함.
+// express 서버실행.
 app.use(express.json());
 // 프론트에서 받은 데이터를 req.body에 넣음
 app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.send("hello express");
-});
-
-app.get("/api", (req, res) => {
-  res.send("hello express API");
-});
-
-app.get("/api/post", (req, res) => {
-  res.json([
-    { id: 2, content: "thdud1" },
-    { id: 3, content: "thdud2" },
-    { id: 4, content: "thdud3" },
-  ]);
-  res.send("hello express API");
-});
+// session, cookie
+app.use(cookieParser("nodebirdsecret"));
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/post", postRouter);
 app.use("/user", userRouter);
