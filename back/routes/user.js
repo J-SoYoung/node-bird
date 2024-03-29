2c7fa3970a71a83054202f762e4cc18ddeb5958d
thdud <thgml1444@naver.com>
2024-03-29 13:37:34 +0900

    Add : 로그인 후 user의 DB에서 가져올 데이터 설정, MySQL을 사용했기 때문에 관계를 지어준 DB table에서 데이터 정보를 추가로 가져올 수 있다. - exclude : password는 user데이터로 프론트에 보내지 않겠다는 설정 - include : Post유저가 쓴 게시글, Followers유저의 팔로우 팔로워들 => as는 user table에서 설정한 대로 작성해줘야 함

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
-const { User } = require("../models");
+const { Post, User } = require("../models");
+
 const router = express.Router();
 
 // POST /user/login
 router.post("/login", (req, res, next) => {
-
   // 미들웨어 확장해서 passport사용하기.
   passport.authenticate("local", (serverError, user, clientError) => {
     if (serverError) {
@@ -23,13 +23,34 @@ router.post("/login", (req, res, next) => {
         console.error(loginErr);
         return next(loginErr);
       }
-
-      return res.status(200).json(user);
+      const fullUserWithoutPassword = await User.findOne({
+        where: { id: user.id },
+        //User 테이블에서 비밀번호 제외하고 정보를 받겠다.
+        attributes: {
+          exclude: ["password"],
+        },
+        // sequalize가 다른 테이블과의 관계를 자동으로 저장해줌
+        // hanMany로 저장했기 때문에 me.posts가됨
+        include: [
+          {
+            model: Post, // 내가 쓴 게시글
+          },
+          {
+            model: User, // follower
+            as: 'Followers',
+          },
+          {
+            model: User,
+            as: 'Followings',
+          },
+        ],
+      });
+      return res.status(200).json(fullUserWithoutPassword);
     });
   })(req, res, next);
 });
 
-// POST /user/
+// POST /user/ (회원가입)
 router.post("/", async (req, res, next) => {
   try {
     const exUser = await User.findOne({
@@ -49,7 +70,6 @@ router.post("/", async (req, res, next) => {
 
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.status(200).send("signup OK");
-
   } catch (error) {
     console.error(error);
     next(error); //status(500)
